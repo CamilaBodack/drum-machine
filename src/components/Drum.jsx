0cd2c2f2e9c77cc88a797930b6a256ccdd62102c
CamilaBodack <camila.bodack@gmail.com>
2020-11-30 16:02:45 -0300

    add className, id and <audio> in Drum component

--- src/components/Drum.jsx
+++ src/components/Drum.jsx
@@ -12,8 +12,19 @@ class Drum extends Component{
             <div>
             <h1>Welcome to Drum machine</h1>
             <DrummerImage />
-            <div id="drum-machine"></div>
+            <div id="drum-machine">
+            <div id="display"></div>
+            <div className = "drum-pad" id="Q"><audio src="https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/80s%20Drum%20Machine/8[kb]80s-Bdrum1.aif"></audio>Q</div>
+            <div className = "drum-pad" id="W"><audio src=""></audio>W</div>
+            <div className = "drum-pad" id="E"><audio src=""></audio>E</div>
+            <div className = "drum-pad" id="A"><audio src=""></audio>A</div>
+            <div className = "drum-pad" id="S"><audio src=""></audio>S</div>
+            <div className = "drum-pad" id="D"><audio src=""></audio>D</div>
+            <div className = "drum-pad" id="Z"><audio src=""></audio>Z</div>
+            <div className = "drum-pad" id="X"><audio src=""></audio>X</div>
+            <div className = "drum-pad" id="C"><audio src=""></audio>C</div>
             </div>
+        </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Drum from './components/Drum'
 
-ReactDOM.render(<Drum />,  document.getElementById('root')
-);
+ReactDOM.render(<Drum/>, document.getElementById('root'));

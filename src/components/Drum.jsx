605b014e957afeb21af0d92e07b4dd6deea471cb
camilabodack <camila.bodack@gmail.com>
2020-12-29 22:08:51 -0300

    change sound information area style and add ascii number to lower case letters

--- src/components/Drum.jsx
+++ src/components/Drum.jsx
@@ -8,62 +8,71 @@ class Drum extends Component {
         this.handleKeyPress = this.handleKeyPress.bind(this)
         this.keyboardLetter = this.keyboardLetter.bind(this)
         this.handleClick = this.handleClick.bind(this)
+
     }
 
-    handleClick(event){
-        let audioID = event.currentTarget.children[event.currentTarget.children.length -1];
-        console.log(event.target.getAttribute("id"))
-
+    handleClick(event) {
+        let audioID = event.currentTarget.children[event.currentTarget.children.length - 1];
         return this.keyboardLetter(audioID.id)
     }
 
     keyboardLetter(letter) {
         console.log('pressed ' + letter)
         let audio = document.getElementById(letter);
+        document.getElementById('display').innerHTML = "------Sound name     : " + audio.parentElement.id + "------";
         audio.play();
     }
 
     handleKeyPress(event) {
         switch (event.keyCode) {
             case 65:
+            case 97:
                 this.keyboardLetter('A')
                 break;
             case 67:
+            case 99:
                 this.keyboardLetter('C')
                 break;
             case 68:
+            case 100:
                 this.keyboardLetter('D')
                 break;
             case 69:
+            case 101:
                 this.keyboardLetter('E')
                 break;
             case 81:
+            case 113:
                 this.keyboardLetter('Q')
                 break;
             case 83:
+            case 115:
                 this.keyboardLetter('S')
                 break;
             case 87:
+            case 119:
                 this.keyboardLetter('W')
                 break;
             case 88:
+            case 120:
                 this.keyboardLetter('X')
                 break;
             case 90:
+            case 122:
                 this.keyboardLetter('Z')
                 break;
             default:
                 console.log('Key not Allowed')
                 break;
         }
+
     }
-
-
 
     render() {
         return (<div>
             <h1>Welcome to Drum machine</h1>
             <DrummerImage/>
+            <h2>Use keyboard keys or click over buttons</h2>
             <div id="drum-machine">
                 <button id="heater-1" className="drum-pad" onClick={this.handleClick} onKeyDown={this.handleKeyPress}>
                     <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>Q</button>
@@ -84,7 +93,7 @@ class Drum extends Component {
                 <button id="fast-kick" className="drum-pad" onClick={this.handleClick} onKeyDown={this.handleKeyPress}>
                     <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>C</button>
             </div>
-            <div id="display">onClick = {document.getElementsByClassName('drum-pad')}</div>
+            <div id="display">Sound name:</div>
 
         </div>)
     }
